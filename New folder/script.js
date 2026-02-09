22dc27e3d99024926b8274bc7ec1d97e9d7746d2
paomedelmetano-hue <paomedelmetano@gmail.com>
2026-02-09 08:18:59 +0800

    Delete New folder directory
    mistake

--- New folder/script.js
+++
@@ -1,7 +0,0 @@
-function toggleForm() {
-    const loginForm = document.getElementById('login-form');
-    const registerForm = document.getElementById('register-form');
-    
-    loginForm.classList.toggle('hidden');
-    registerForm.classList.toggle('hidden');
-}
